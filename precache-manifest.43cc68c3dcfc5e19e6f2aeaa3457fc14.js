080cdae6130666d09e9ce326bcab527bba831b9b
Kaushik Raj <kaushikrajn@gmail.com>
2021-09-19 21:24:54 +0530

    Updates

---
+++ precache-manifest.43cc68c3dcfc5e19e6f2aeaa3457fc14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52e38df505673d1c0240b2c6a8d635b",
+    "url": "/home-iitk-profilepage/index.html"
+  },
+  {
+    "revision": "7ba75f1008ac51db2eac",
+    "url": "/home-iitk-profilepage/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "91c44305bea780502bc4",
+    "url": "/home-iitk-profilepage/static/css/main.fcf2461e.chunk.css"
+  },
+  {
+    "revision": "7ba75f1008ac51db2eac",
+    "url": "/home-iitk-profilepage/static/js/2.31e1cfa3.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/home-iitk-profilepage/static/js/2.31e1cfa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c44305bea780502bc4",
+    "url": "/home-iitk-profilepage/static/js/main.ac7ae7af.chunk.js"
+  },
+  {
+    "revision": "1b21649f20a40d9497e8",
+    "url": "/home-iitk-profilepage/static/js/runtime-main.76706f0f.js"
+  }
+]);
